5481e3572d056e5302a25ece2dff699f32ea71e2
Phan Truong <phantruong0725@gmail.com>
2024-10-26 15:30:03 +0700

    vua

---
+++ src/main/java/PBL4/Server.java
@@ -0,0 +1,17 @@
+package PBL4;
+
+public class Server {
+    private static final int port = 5504;
+    private static final int a = 0;
+
+
+
+
+    static class ClientHandler implements Runnable{
+
+        @Override
+        public void run() {
+
+        }
+    }
+}
